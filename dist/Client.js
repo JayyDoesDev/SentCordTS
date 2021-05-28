9be35e42f052a97841aabe572fa7115619b36d9d
SixAiy <38549893+SixAiy@users.noreply.github.com>
2021-05-28 19:09:49 +0800

    Update Client.js

--- dist/Client.js
+++ dist/Client.js
@@ -14,13 +14,14 @@ class sentcordTS {
             throw new ReferenceError('In order to post your stats you need to pass in your API key. To access the api key do command ..apikey in the support server.');
         if (!botid)
             throw new ReferenceError('In order to post your stats you need to pass in your bot id.');
-        this.fetch(`https://sentcord.com/api/bot/${botid}`, {
+        this.fetch(`https://api.sixaiy.com/v2/sentcord`, {
             method: 'POST',
             headers: {
                 'Authorization': this.apikey,
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
+                id: botid,
                 serverCount: servers,
                 shardCount: shards
             })
