17fb3409c7f9decc27ad198b9dcc28bc9fafc709
Jdevdisc <69683068+JDevelo@users.noreply.github.com>
2021-06-04 22:14:41 -0400

    API Changes

--- dist/structure/lib/_stats.js
+++ dist/structure/lib/_stats.js
@@ -9,21 +9,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 module.exports = (_botid, _servers, _shards, _apikey) => __awaiter(void 0, void 0, void 0, function* () {
+    const verison = require('../../../src/structure/lib/json/version.json');
     const fetch = require('node-fetch');
     if (!_apikey)
         throw new ReferenceError('In order to post your stats you need to pass in your API key. To access the api key do command ..apikey in the support server.');
     if (!_botid)
         throw new ReferenceError('In order to post your stats you need to pass in your bot id.');
-    fetch(`https://sentcord.com/api/bot/${_botid}`, {
+    fetch(`https://api.sixaiy.com/${verison.api.Version.v2}/sentcord`, {
         method: 'POST',
         headers: {
             'Authorization': _apikey,
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
+            id: _botid,
             serverCount: _servers,
             shardCount: _shards
         })
     });
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiX3N0YXRzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vc3JjL3N0cnVjdHVyZS9saWIvX3N0YXRzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7QUFBQSxpQkFBUyxDQUFPLE1BQWUsRUFBRSxRQUFpQixFQUFFLE9BQWdCLEVBQUUsT0FBYSxFQUFFLEVBQUU7SUFDbkYsTUFBTSxLQUFLLEdBQUcsT0FBTyxDQUFDLFlBQVksQ0FBQyxDQUFBO0lBQ25DLElBQUcsQ0FBQyxPQUFPO1FBQUUsTUFBTSxJQUFJLGNBQWMsQ0FBQyxnSUFBZ0ksQ0FBQyxDQUFBO0lBQ3ZLLElBQUcsQ0FBQyxNQUFNO1FBQUUsTUFBTSxJQUFJLGNBQWMsQ0FBQyw4REFBOEQsQ0FBQyxDQUFBO0lBQ3BHLEtBQUssQ0FBQyxnQ0FBZ0MsTUFBTSxFQUFFLEVBQUU7UUFDNUMsTUFBTSxFQUFFLE1BQU07UUFDVixPQUFPLEVBQUU7WUFDTCxlQUFlLEVBQUUsT0FBTztZQUN4QixjQUFjLEVBQUUsa0JBQWtCO1NBQ3JDO1FBQ0QsSUFBSSxFQUFFLElBQUksQ0FBQyxTQUFTLENBQUM7WUFDakIsV0FBVyxFQUFFLFFBQVE7WUFDckIsVUFBVSxFQUFFLE9BQU87U0FDdEIsQ0FBQztLQUNULENBQUMsQ0FBQTtBQUNOLENBQUMsQ0FBQSxDQUFBIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiX3N0YXRzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vc3JjL3N0cnVjdHVyZS9saWIvX3N0YXRzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7QUFBQSxpQkFBUyxDQUFPLE1BQWUsRUFBRSxRQUFpQixFQUFFLE9BQWdCLEVBQUUsT0FBYSxFQUFFLEVBQUU7SUFDbkYsTUFBTSxPQUFPLEdBQUcsT0FBTyxDQUFDLHFCQUFxQixDQUFDLENBQUE7SUFDOUMsTUFBTSxLQUFLLEdBQUcsT0FBTyxDQUFDLFlBQVksQ0FBQyxDQUFBO0lBQ25DLElBQUcsQ0FBQyxPQUFPO1FBQUUsTUFBTSxJQUFJLGNBQWMsQ0FBQyxnSUFBZ0ksQ0FBQyxDQUFBO0lBQ3ZLLElBQUcsQ0FBQyxNQUFNO1FBQUUsTUFBTSxJQUFJLGNBQWMsQ0FBQyw4REFBOEQsQ0FBQyxDQUFBO0lBQ3BHLEtBQUssQ0FBQywwQkFBMEIsT0FBTyxDQUFDLEdBQUcsQ0FBQyxPQUFPLENBQUMsRUFBRSxXQUFXLEVBQUU7UUFDL0QsTUFBTSxFQUFFLE1BQU07UUFDVixPQUFPLEVBQUU7WUFDTCxlQUFlLEVBQUUsT0FBTztZQUN4QixjQUFjLEVBQUUsa0JBQWtCO1NBQ3JDO1FBQ0QsSUFBSSxFQUFFLElBQUksQ0FBQyxTQUFTLENBQUM7WUFDakIsRUFBRSxFQUFFLE1BQU07WUFDVixXQUFXLEVBQUUsUUFBUTtZQUNyQixVQUFVLEVBQUUsT0FBTztTQUN0QixDQUFDO0tBQ1QsQ0FBQyxDQUFBO0FBQ04sQ0FBQyxDQUFBLENBQUEifQ==
--- src/structure/lib/_stats.ts
+++ src/structure/lib/_stats.ts
@@ -1,14 +1,16 @@
 export = async (_botid?: string, _servers?: string, _shards?: string, _apikey?: any) => {
+    const verison = require('./json/version.json')
     const fetch = require('node-fetch')
     if(!_apikey) throw new ReferenceError('In order to post your stats you need to pass in your API key. To access the api key do command ..apikey in the support server.')
     if(!_botid) throw new ReferenceError('In order to post your stats you need to pass in your bot id.')
-    fetch(`https://sentcord.com/api/bot/${_botid}`, {
+    fetch(`https://api.sixaiy.com/${verison.api.Version.v2}/sentcord`, {
         method: 'POST',
             headers: {
                 'Authorization': _apikey,
                 'Content-Type': 'application/json'
-            },
+            },                                
             body: JSON.stringify({
+                id: _botid,
                 serverCount: _servers,
                 shardCount: _shards
             })
